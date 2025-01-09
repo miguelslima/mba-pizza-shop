12490e9522161a9953132c3cd8eb140419458a33
Miguel Lima <miguelsousalima@hotmail.com>
2025-01-09 07:42:21 -0300

    Project initial and shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+import "./index.css";
+
+export function App() {
+  return (
+    <>
+      <h1>Pizza</h1>
+      <Button variant="destructive">Button</Button>
+    </>
+  );
+}
