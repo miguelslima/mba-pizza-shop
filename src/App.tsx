90f4caec64ac8e56db8d432618ec7af148b26cf8
Miguel Lima <miguelsousalima@hotmail.com>
2025-01-09 07:56:25 -0300

    Config eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./index.css";
 
 export function App() {
   return (
-    <>
+    <div className="flex h-screen flex-col items-center justify-center">
       <h1>Pizza</h1>
       <Button variant="destructive">Button</Button>
-    </>
+    </div>
   );
 }
