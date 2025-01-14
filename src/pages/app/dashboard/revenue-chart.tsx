1da6474bcc76f477151a27a3be0976dbae244fc6
Miguel Lima <miguelsousalima@hotmail.com>
2025-01-14 18:51:13 -0300

    Popular Products Chart

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,4 +1,11 @@
-import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 import colors from "tailwindcss/colors";
 
@@ -55,6 +62,8 @@ export function RevenueChart() {
               strokeWidth={2}
               dataKey="revenue"
             />
+
+            <CartesianGrid vertical={false} className="stroke-muted" />
           </LineChart>
         </ResponsiveContainer>
       </CardContent>
