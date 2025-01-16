fbf04f7e8fa94656fe91d40892c9f74b86216076
Miguel Lima <miguelsousalima@hotmail.com>
2025-01-15 23:11:51 -0300

    Config api client

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Toaster } from "sonner";
 
 import { router } from "./routes";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
@@ -15,7 +17,9 @@ export function App() {
 
         <Toaster richColors />
 
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
